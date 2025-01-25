2a6b979c1f65398f775be7187f6d5ae206eb959f
Bigya <bigyabajracharya@gmail.com>
2025-01-24 23:13:29 -0500

    Backend packages setup and add few models

---
+++ backend/models/Transaction.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    type: { type: String, enum: ["income", "expense", "saving"], required: true },
+    description: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
---
+++ backend/models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firebaseId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
