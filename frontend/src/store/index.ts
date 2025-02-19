3df802d1918a490034b7d2d6a8142556a859f25d
Bigya <bigyabajracharya@gmail.com>
2025-02-18 23:59:11 -0500

    Set up store for redux

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import transactionReducer from "./reducers/transactionSlice";
+import userReducer from "./reducers/userSlice";
+
+const store = configureStore({
+  reducer: {
+    transactions: transactionReducer,
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
---
+++ frontend/src/store/reducers/transactionSlice.tsx
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Transaction {
+  id: string;
+  type: "income" | "expense" | "saving";
+  description: string;
+  amount: number;
+  date: string;
+}
+
+interface TransactionsState {
+  transactions: Transaction[];
+}
+
+const initialState: TransactionsState = {
+  transactions: [],
+};
+
+const transactionSlice = createSlice({
+  name: "transactions",
+  initialState,
+  reducers: {
+    addTransaction: (state, action: PayloadAction<Transaction>) => {
+      state.transactions.push(action.payload);
+    },
+    removeTransaction: (state, action: PayloadAction<string>) => {
+      state.transactions = state.transactions.filter(
+        (transaction) => transaction.id !== action.payload
+      );
+    },
+    setTransactions: (state, action: PayloadAction<Transaction[]>) => {
+      state.transactions = action.payload;
+    },
+  },
+});
+
+export const { addTransaction, removeTransaction, setTransactions } = transactionSlice.actions;
+export default transactionSlice.reducer;
